3344c483e8bd9972baf15c8556f69b1f06e8fb25
Ryan Dawkes <rd@moose.co>
2021-05-01 23:54:51 +0100

    setup home & portfolio page templates

---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import { About } from '../components/home/About';
+import { Contact } from '../components/home/Contact';
+import { Portfolio } from '../components/home/Portfolio';
+
+export const Home = (props) => {
+  const hh = props.hh;
+  const fh = props.fh;
+  const wh = window.innerHeight;
+
+  const myArr = [...document.getElementsByName("section")];
+  myArr.forEach(e => {
+    if (e.id === "portfolio-home") e.style.minHeight = `${wh - hh}px`;
+    if (e.id === "about") e.style.minHeight = `${wh}px`;
+    if (e.id === "contact") e.style.minHeight = `${wh - fh}`;
+  });
+
+  return (
+    <div id="home-container">
+
+      <Portfolio />
+      <About />
+      <Contact />
+
+    </div>
+  )
+}
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import data from '../data.json';
+
+export const Portfolio = () => {
+
+  console.log(data)
+  
+  return (
+    <div id="portfolio-container">
+      Hello
+    </div>
+  )
+}
