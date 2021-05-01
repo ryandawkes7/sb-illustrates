b998755f3ff6d38eee314b9df942a1641b939acc
Ryan Dawkes <rd@moose.co>
2021-05-01 23:51:06 +0100

    setup routes && pass height props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import './styles/App.scss';
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
+import "./styles/main.scss";
+import { Header } from "./components/globals/Header";
+import { Footer } from "./components/globals/Footer";
+import React, {useEffect, useRef, useState} from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Portfolio } from "./pages/Portfolio";
 
 function App() {
-  return (
 
-    <div>
-      <Header />
-      <div>Hello world</div>
-      <Footer />
-    </div>
+  const headerRef = useRef(null);
+  const footerRef = useRef(null);
+
+  const [headerH, setHeaderH] = useState(null);
+  const [footerH, setFooterH] = useState(null);
+
+  useEffect(() => {
+    // Header height
+    if (headerRef.current) {
+     let hh = headerRef.current.offsetHeight;
+      setHeaderH(hh);
+    }
+
+    // Footer height
+    if (footerRef.current) {
+      let fh = footerRef.current.offsetHeight;
+      setFooterH(fh);
+    }
+  }, [headerRef, footerRef])
+
+  return (
+    <Router>
+        <Header headerRef={headerRef} />
+
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <Home hh={headerH} fh={footerH}/>
+            </Route>
+            <Route exact path="/portfolio">
+              <Portfolio hh={headerH} fh={footerH}/>
+            </Route>
+          </Switch>
+        </main>
+
+        <Footer footerRef={footerRef} />
+    </Router>
   );
 }
 
