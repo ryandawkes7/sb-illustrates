068b52f52fd1e8750560b31338001d926f7c902b
Ryan Dawkes <rd@moose.co>
2021-05-03 22:40:08 +0100

    update profile image sizing & position

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -5,8 +5,14 @@ export const About = () => {
     <div id="about" className="section_container w-100 d-flex align-items-center" name="section">
       <div className="d-flex justify-content-center align-items-center w-100 px-100 about_container">
 
+        {/* Profile image */}
+        <div className="col-12 col-md-6 mr-auto p-3 d-flex align-items-center justify-content-center">
+            <img className="profile_image w-75 d-none d-md-block mr-auto" src={`${process.env.PUBLIC_URL}/assets/profile-image.png`} alt="Self Portrait" />
+            <img className="profile_image w-100 d-block d-md-none mr-auto" src={`${process.env.PUBLIC_URL}/assets/profile-image.png`} alt="Self Portrait" />
+        </div>
+
         {/* Description */}
-        <div className="col-12 col-md-8 pr-100 d-flex justify-content-center flex-column description_container">
+        <div className="col-12 col-md-6 pr-100 d-flex justify-content-center flex-column description_container">
             <h1 className="p-2">About Me</h1>
             <p className="p-2">
                 Ny name is Sarah Brennan. I am a third-year Illustration (BA Hons) student
@@ -14,11 +20,6 @@ export const About = () => {
                 portrait-based artwork, specifically from a shoulder-up perspective, 
                 but I do not shy away from alternative mediums.
             </p>
-        </div>
-
-        {/* Profile image */}
-        <div className="col-10 col-md-4 p-3 d-flex align-items-center justify-content-center">
-            <img className="profile_image w-100" src={`${process.env.PUBLIC_URL}/assets/profile-image.png`} alt="Self Portrait" />
         </div>
 
       </div>
