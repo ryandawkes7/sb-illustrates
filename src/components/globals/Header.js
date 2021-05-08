09a59d10769f7dc467b4b9d2b4f8134711bf4413
Ryan Dawkes <rd@moose.co>
2021-05-08 15:59:20 +0100

    update nav dropdown

--- src/components/globals/Header.js
+++ src/components/globals/Header.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Nav, Navbar } from "react-bootstrap";
 
 export const Header = (props) => {
   const [expand, setExpand] = useState(false);
@@ -7,9 +7,9 @@ export const Header = (props) => {
   const navExpand = () => {
     setExpand(true);
   };
-  // const navClose = () => {
-  //   setExpand(false);
-  // };
+  const navClose = () => {
+    setExpand(false);
+  };
 
   const headerRef = props.headerRef;
   const publicPath = process.env.PUBLIC_URL;
@@ -18,46 +18,60 @@ export const Header = (props) => {
     <header id="header" ref={headerRef}>
       <div className="navbar navbar-light navbar-backdrop">
         <nav className="navbar navbar-expand-lg w-100">
-          <Navbar
-            id="header"
-            className="w-100 d-flex"
-            expand="lg"
-            expanded={expand}
-            onToggle={navExpand}
-          >
-            {/* Brand */}
+          <Navbar expand="lg" id="header" className="w-100 d-none d-lg-flex">
             <Navbar.Brand href="/">
               <img
                 src={`${publicPath}/assets/logo-wide.png`}
                 alt="SB Illustrates"
               />
             </Navbar.Brand>
-
-            {/* Toggler */}
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
-            {/* Collapsable Content */}
-            <Navbar.Collapse
-              id="basic-navbar-nav"
-              className="d-flex ml-auto"
-              style={{ width: 'fit-content' }}
-            >
+            <Nav className="ml-auto">
+              <Nav.Link
+                className="nav-link mauve-color font-weight-bold px-3"
+                href="/"
+              >
+                home
+              </Nav.Link>
+              <Nav.Link
+                className="nav-link mauve-color font-weight-bold px-3"
+                href="/portfolio"
+              >
+                portfolio
+              </Nav.Link>
+              <Nav.Link
+                className="nav-link mauve-color font-weight-bold px-3"
+                href="/contact"
+              >
+                contact
+              </Nav.Link>
+            </Nav>
+          </Navbar>
+          <Navbar expand="lg" id="header" className="w-100 d-flex d-lg-none">
+            <Navbar.Brand href="/">
+              <img
+                src={`${publicPath}/assets/logo-wide.png`}
+                alt="SB Illustrates"
+              />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="mt-4">
               <Nav className="mr-auto">
                 <Nav.Link
-                  className="nav-link mauve-color font-weight-bold"
+                  className="nav-link mauve-color font-weight-bold px-3 my-2"
                   href="/"
                 >
                   home
                 </Nav.Link>
                 <Nav.Link
-                  className="nav-link mauve-color font-weight-bold"
-                  href="#link"
+                  className="nav-link mauve-color font-weight-bold px-3 my-2"
+                  href="/portfolio"
                 >
                   portfolio
                 </Nav.Link>
                 <Nav.Link
-                  className="nav-link mauve-color font-weight-bold"
-                  href="/"
+                  className="nav-link mauve-color font-weight-bold px-3 my-2"
+                  href="/contact"
                 >
                   contact
                 </Nav.Link>
